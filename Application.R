e19e70984434ad6bcd9ea4453319654046df18a1
royarkaprava <arkapravaroy0075@gmail.com>
2023-03-29 22:32:36 -0400

    Update Application.R

--- Application.R
+++ Application.R
@@ -9,14 +9,14 @@ local_bvals_path = "data/cfin_multib/bval.bval"
 
 local_bvecs_path <- "data/cfin_multib/bvec.bvec"
 
-pred_bvals<- "HCP_protocol/bvals"
+pred_bvals_path <- "HCP_protocol/bvals"
 
-pred_bvecs="HCP_protocol/bvecs"
+pred_bvecs_path <- "HCP_protocol/bvecs"
 
 #Predicted on HCP grid
 out <- DWI_Processing(data_path=datapath,
                       local_bvals_path,local_bvecs_path,
-                      pred_bvals_path=pred_bvals,pred_bvecs_path=pred_bvecs,
+                      pred_bvals_path=pred_bvals_path,pred_bvecs_path=pred_bvecs_path,
                       order=4,Mb=10, sig = 0.01)
 
 #Fitted signal on its own grid of bvals and bvecs
